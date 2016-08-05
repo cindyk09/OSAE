b039e0ccd9e1f80032459f9d8be126e81aa50cff
Cindy <cindyk09@gmail.com>
2016-08-05 15:18:14 -0400

    routes are working

--- app.js
+++ app.js
@@ -7,10 +7,11 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var path = require('path');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+// var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
@@ -19,10 +20,32 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+// var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+// app.listen(appEnv.port, '0.0.0.0', function() {
+//   // print a message when the server starts listening
+//   console.log("server starting on " + appEnv.url);
+// });
+
+app.get('/', function(req, res){
+  // res.sendFile takes an absolute path to a file and
+  // sets the mime type based n the file extname
+  res.sendFile(__dirname + '/index.html', function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  })
 });
+
+app.get('/application',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/application.html'));
+});
+
+app.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/sitemap.html'));
+});
+
+
+app.listen(3000);
+console.log("Running at Port 3000");
