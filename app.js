10aea2569c7c7644d599c8e4cd14f93b2054f676
Cindy <cindyk09@gmail.com>
2016-08-10 15:11:06 -0400

    nav bar sign up and log in. Transitioning color. Button from get involved to learn more

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');
 var path = require('path');
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+// var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 // serve the files out of ./public as our main files
@@ -27,12 +27,12 @@ app.get('/application',function(req,res){
   res.sendFile(path.join(__dirname+'/public/application.html'));
 });
 
-var appEnv = cfenv.getAppEnv();
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
+// var appEnv = cfenv.getAppEnv();
+// // start server on the specified port and binding host
+// app.listen(appEnv.port, '0.0.0.0', function() {
+//   // print a message when the server starts listening
+//   console.log("server starting on " + appEnv.url);
+// });
 
-// app.listen(3000);
-// console.log("Running at Port 3000");
+app.listen(3000);
+console.log("Running at Port 3000");
