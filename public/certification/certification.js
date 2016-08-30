bd393c1988c99ccb685ff325266e0b4653811056
Cindy <cindyk09@gmail.com>
2016-08-30 13:27:45 -0400

    fixed the os add and remove

--- public/certification/certification.js
+++ public/certification/certification.js
@@ -3,53 +3,54 @@ $(document).ready(function() {
         var i = 1;
         var a = 1;
         var o = 1;
-        var osDiv = $('#os_class')
+        var osDiv = $('#osClass')
 
+// Add more contacts
         $(document).on('click', "#contact_button",function() {
                 i++;
-                $('<div class="extra_contact_'+i+'"><li><p class="left"><label for="contact_name">First Name<span class="req">*</span></label><input type="text" name="first_name_'+i+'"/></p><p class="pull-right"><label for="contact_name">Last Name<span class="req">*</span></label><input type="text" name="last_name_'+i+'" /></p></li><li><p><label for="company">Email <span class="req">*</span></label><input type="email" name="email_'+i+'" /></p><p class="right"><label for="contact_name">Position<span class="req">*</span></label><input type="text" name="position_1" placeholder="Ex: Software Engineer" required/></li><hr></div>').appendTo(scntDiv);
+                $('<div class="extra_contact_'+i+'"><li><p class="left"><label for="contact_name">First Name<span class="req">*</span></label><input type="text" name="first_name_'+i+'"/></p><p class="pull-right"><label for="contact_name">Last Name<span class="req">*</span></label><input type="text" name="last_name_'+i+'" /></p></li><li><p><label for="company">Email <span class="req">*</span></label><input type="email" name="email_'+i+'" /></p><p class="right"><label for="contact_name">Position<span class="req">*</span></label><input type="text" name="position_1" placeholder="Ex: Software Engineer" required/></li></div>').appendTo(scntDiv);
                 return false;
         });
 
-        // $(document).on('click', '#remScnt', function() {
-        //         if( i > 1 ) {
-        //                 $(this).parents('div').first().remove();
-        //                 i--;
-        //         }
-        //         return false;
-        // });
-
+// remove last added contacts
         $(document).on('click', '#remScnt', function() {
                 if( i > 1 ) {
-                        // $(this).parents('#contacts div').remove();
-                        $('#contacts div:last').remove();
+                        $('scntDiv div:last').remove();
                         i--;
                 }
                 return false;
         });
 
+// show remove button only when there are more than 1
+        $('#remScnt').hide();
 
-
-
-            $('#remScnt').hide();
         $(document).on('click', '#contact_button', function() {
                   $('#remScnt').show();
                 return false;
         });
+
+// Add more on os version
         $(document).on('click','#os_button', function() {
                 a++;
-                $('<div id="os_info_'+a+'"><p><input type="text"  name="os_info_'+a+'" placeholder="OS distribution, version" /><a id="remOS"> Remove</a></p><br></div>').appendTo(osDiv);
+                $('<div class="os_info_'+a+'"><p><input type="text"  name="os_info_'+a+'" placeholder="OS distribution, version"/></p></div>').appendTo(osDiv);
 
                 return false;
         });
+
+// remove last added os
         $(document).on('click', '#remOS', function() {
-                // text_box='#os_info_'+a
                 if( a > 1 ) {
-                        $(this).parents(text_box).remove();
+                        $('#osClass div:last').remove();
                         a--;
                 }
                 return false;
         });
 
+  // show remove button only when there are more than 1
+          $('#remOS').hide();
 
+          $(document).on('click', '#os_button', function() {
+                    $('#remOS').show();
+                  return false;
+          });
 });
